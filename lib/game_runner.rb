fe2c32cd4eceba7b40c7aba4b8ea41047b128448
Kelvin Samuel <kelvin@kks110.com>
2019-01-11 21:43:47 +0000

    Reduced down the code to smaller looks, reduces amount of files and steps needed

--- lib/game_runner.rb
+++ lib/game_runner.rb
@@ -9,20 +9,20 @@ class GameRunner
   def self.run!
     # Get the player name.
     player = Character.new
+
     # Sets the first level.
-    @level = "forest"
-    # Starts the game at the first level.
-    self.send(@level, player)
+    @next_step_and_level = ["wakeup", "Forest"]
+
+    # Starts the logic loop
+    level_start(player, @next_step_and_level)
   end
 
-  # The journey begins. In to the forest.
-  def self.forest(player)
-    # Sets the level = to what is returned from the forest
-    # to see where the player goes next.
-    @level = Forest.run!(player)
-
-    # This will send the player to the next level once its written. 
-    # self.send(@level, player)
+  def self.level_start(player, step_and_level)
+    level = Object.const_get(step_and_level[1])
+    while step_and_level[0] != "next_level" do
+      step_and_level = level.send(step_and_level[0], player.name)
+    end
+    return step_and_level
   end
 
 
--- lib/levels/forest.rb
+++ lib/levels/forest.rb
@@ -1,32 +1,86 @@
-require 'forest_logic'
-
 class Forest
 
-  attr_accessor :forest_complete
-
-  # Will to be used to keep track of when the level is complete
-  # and where to send the player to next.
-  @forest_complete = "forest"
-
-  # Calls the forest logic.
-  def self.run!(player)
-    # The forest logic will return a word if the player leaves the forest.
-    # That will be used to break the loop and send the player back to the game_runner
-    while @forest_complete == "forest" do
-      # Start up in the forest.
-      ForestLogic.wakeup(player.name)
-
-      # Player makes their first decision. To go left or right.
-      forest_decision = ForestLogic.left_or_right
-
-      # The result of this decision could complete this loop here, or send them fruther in to the forest.
-      # If they exit at this point, it will return "town" as the next location.
-      @forest_complete = ForestLogic.send(forest_decision)
-
-
-    end
-    # Will return the next location, so the game knows where it needs to go next.
-    return @forest_complete
+  # Player starts here, waking up in the forest.
+  def self.wakeup(player_name)
+    puts "You see the sun creeping through your closed eyes."
+    gets
+    puts "As you begin to open them, you realised you are no longer in your warm bed."
+    gets
+    puts "As you look around you see trees. How did you get here?"
+    gets
+    puts "You seem to be fully dressed."
+    gets
+    puts "'Come on #{player_name}, lets get up!' You think to yourself."
+    gets
+    return "left_or_right", "Forest"
   end
+
+  # The first decision, left or right.
+  def self.left_or_right(player_name)
+    puts "You look around and see a path near by."
+    gets
+    puts "You cannot see any sign posts and you dont recognise the area"
+    gets
+    puts "You can either go left or right. Which way do you want to go?: "
+    loop do
+      puts "1 - Left"
+      puts "2 - Right"
+      l_or_r = gets.chomp
+      # It will return what they pick to send them to the next step.
+      if l_or_r == "1"
+        return "left", "Forest"
+      elsif l_or_r == "2"
+        return "right", "Forest"
+      else
+        puts "That was not a valid option, please enter 1 or 2': "
+      end
+    end
+  end
+
+  # If they picked left, they are out, and are heading to the town.
+  def self.left(player_name)
+    puts "You walk fruther and the trees begin to thin out."
+    gets
+    puts "You start to see buildings through the trees."
+    gets
+    puts "You emerge from the forest and before you is a town."
+    puts "A signpost nearby points in two directions. One to the forst and says 'Rackstone Forest'"
+    puts "The other to the town and says 'High Church'. You don't recognise either of these names"
+    return "next_level", "Town"
+  end
+
+  # If not, their journey continues.
+  def self.right(player_name)
+    puts "You start your journey along this road to see where it goes."
+    gets
+    puts "As you continue, the forest seems to get thicker arround you."
+    gets
+    puts "Off to the side of the path, you see smoke rishing. As you get closer you realise its a house."
+    gets
+    puts "You come closer to the front of the house."
+    gets
+    loop do
+      puts "1 - Walk past"
+      puts "2 - Knock on the door"
+      l_or_r = gets.chomp
+      # It will return what they pick to send them to the next step.
+      if l_or_r == "1"
+        return "go_past", "Forest"
+      elsif l_or_r == "2"
+        return "knock", "Forest"
+      else
+        puts "That was not a valid option, please enter 1 or 2': "
+      end
+    end
+  end
+
+  def self.go_past(player_name)
+  end
+
+  def self.knock(player_name)
+  end
+
+
+
 
 end
